788c92e290614979eae71f8a5b8bda3c6349bc6a
Gleb Zinoviev <gz0609@gmail.com>
2020-02-26 22:38:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    var modulN = Math.abs(n);
+    var stringN = modulN.toString();
+    var reverseN = stringN.split('').reverse().join('');
+    var a = Number(reverseN);
+    return (a);
 }
